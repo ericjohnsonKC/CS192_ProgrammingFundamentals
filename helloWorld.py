7e4b71363293e3d72b785a30fe6c2464b39dd4f7
ericj <ericjohnson.kc@gmail.com>
2020-02-17 18:24:43 -0600

    Committing .idea files

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,48 @@
-print("Hello World")
+# class User():
+#     def __init__(self, username, password):
+#         self.username = username
+#         self.password = password
+#         self.online = False
+#
+#     def login(self):
+#         self.online = True
+#         print('You are logged in as:)
+#
+#
+#
+# def mainMenu():
+#     menuHeader = '{:.^30}'.format("Main Menu")
+#     print(menuHeader)
+#     print("1. Login")
+#     print("2. Logout")
+#     print("3. Create new account")
+#     print("Enter 'q' at any time to exit...")
+#     selection = input('\nSelection>')
+#     return selection
+#
+# def login():
+#     return 0
+#
+# def logout():
+#     return 0
+#
+# def createUser():
+#     return 0
+#
+#
+#
+#
+#
+# selection = mainMenu()
+# while selection != 'q' and selection != 'Q':
+#     if selection == '1':
+#         user.login()
+#     elif selection == '2':
+#         logout()
+#     elif selection == '3':
+#         createUser()
+#     else:
+#         print('Please select a valid menu option')
+#     selection = mainMenu()
+
+
